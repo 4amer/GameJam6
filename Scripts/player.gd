d8bc7e73e96f7353ec335b34efb42fe0fbbdb778
Ivan Horunov <ivan.horunov@elephant-games.am>
2024-12-06 13:03:42 +0300

    player movement added

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+@export var bullet_speed = 1000;
+@export var projectile_scene = preload("res://Nodes/Projectiles/projectile_coal.tscn");
 
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO;
@@ -16,7 +18,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
+	if Input.is_action_just_pressed("Shoot"):
+		spawn_projectile();
+	
 	velocity = velocity.normalized();
 	velocity *= SPEED;
 	
+	$aim.look_at(get_global_mouse_position())
+	
 	move_and_slide()
+
+func spawn_projectile() -> void:
+	var bullet_instance = projectile_scene.instantiate();
+	bullet_instance.position = $aim/shooterPoint.global_position;
+	bullet_instance.rotation_degrees = $aim.rotation_degrees;
+	bullet_instance.apply_impulse(Vector2(bullet_speed, 0).rotated($aim.rotation))
+	get_tree().get_root().call_deferred("add_child", bullet_instance);
+	
